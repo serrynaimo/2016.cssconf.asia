0f79e997c26e29cd97fb73f77467a76fc21591ef
serrynaimo <serrynaimo@me.com>
2016-11-30 14:04:51 +1100

    link video playlist

--- index.php
+++ index.php
@@ -39,11 +39,10 @@
                 Capitol Theatre, Singapore
             </p>
             <p>
-                CSSConf.Asia starts<br/>
-                <span id="countdown">&nbsp;</span>
+                Thank you for supporting CSSConf.Asia!<br/>
             </p>
-            <a href="https://www.eventnook.com/event/devfestasia2016/home" target="_blank" class="button">
-                Get your tickets
+            <a href="https://www.youtube.com/watch?v=hj355PRbwSQ&list=PL37ZVnwpeshFizH1JsG3s7_qyN2Uk0OSh" target="_blank" class="button">
+                Watch the videos!
             </a>
 	    </div>
 	</section>
@@ -345,7 +344,7 @@
   ga('send', 'pageview');
 
 
-   CountDownTimer('Thu, 24 Nov 2016 09:00:00 +0800', 'countdown');
+   // CountDownTimer('Thu, 24 Nov 2016 09:00:00 +0800', 'countdown');
 
     function CountDownTimer(dt, id)
     {
